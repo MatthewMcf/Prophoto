45b6305be8eb58d8a1879605dccec03d6d6aba3d
harryjsutcliffe <harryjamessutcliffe@gmail.com>
2022-03-17 16:43:05 +0900

    add barebones menu bar for logged in and logged out

--- view/menu.php
+++ view/menu.php
@@ -1,1 +1,37 @@
-<div>Menu to insert here</div>
+<header>
+
+    <?php $logged_in = TRUE;
+    
+    if ($logged_in) {
+    ?>
+    
+    <!-- Logged In -->
+    <div id="menu-bar" class="logged-in">
+    <div><img src="./public/images/ProPhoto.png" alt="Pro Photo Logo" id="site-logo"></div>
+    <h1>Pro Photo</h1>
+    <nav>
+        <li>My Profile</li>
+        <li>Log Out</li>
+    </nav>
+    </div>
+
+
+
+    <?php } else {        
+    ?>
+
+    <!-- Logged Out -->
+    <div id="menu-bar">
+    <div><img src="./public/images/ProPhoto.png" alt="Pro Photo Logo" id="site-logo"></div>
+    <h1>Pro Photo</h1>
+    <nav>
+        <li>Register</li>
+        <li>Log In</li>
+    </nav>
+    </div>
+
+
+
+    <?php } ?>
+</header>
+
