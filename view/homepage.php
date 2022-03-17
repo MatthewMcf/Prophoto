6f80049b853c941dbaf02fc0e464433107f799ff
harryjsutcliffe <harryjamessutcliffe@gmail.com>
2022-03-17 15:29:47 +0900

    add divs for call-to-action, search bar, popular tags, and profile information

--- view/homepage.php
+++ view/homepage.php
@@ -1,4 +1,28 @@
 <?php ob_start();?>
-<div>MAIN PAGE</div>
+    <h1>Pro Photo</h1>
+
+	<div id="call-to-action">
+		Placeholder Call-To-Action.
+	</div>
+	<div id="call-to-action-secondary">
+		Secondary Call-To-Action text.
+	</div>
+	
+	<div class="search-bar">
+		<input type="text" placeholder="Search for images">
+		<button type="submit">Search</button>
+	</div>
+
+	<div id="popular-tags">
+	
+	</div>
+
+    <!-- Logged In -->
+
+    <div id="profile-information">
+
+    </div>
+
+
 <?php $content = ob_get_clean();?>
 <?php require('template.php');?>
