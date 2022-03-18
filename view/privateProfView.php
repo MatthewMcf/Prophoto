8d1bef8c6938bc2227e8968f047fe0b9a4d7afaa
leegihe95 <leegihe95@gmail.com>
2022-03-18 14:10:25 +0900

    private Profile View creation

---
+++ view/privateProfView.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../public/css/privateProfView.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <section>
+        <div id="profileHeader">
+            <div id="profilePic">
+                <img src="../public/images/default_profile_picture.png" alt="profilePic">
+            </div>
+            <p>My profile information</p>
+            <button>Edit</button>
+        </div>
+        <div id="profileAlbums">
+            <div class="album">
+                <div class="albumShadow">
+                    <div>
+                        <button>Edit</button>
+                    </div>
+                </div>
+            </div>
+            <div class="album">
+                <div class="albumShadow">
+                    <div>
+                        <button>Edit</button>
+                    </div>
+                </div>
+            </div>
+            <div class="album">
+                <div class="albumShadow">
+                    <div>
+                        <button>Edit</button>
+                    </div>
+                </div>
+            </div>
+            <div class="album">
+                <div class="albumShadow">
+                    <div>
+                        <button>Edit</button>
+                    </div>
+                </div>
+            </div>
+            <div class="album">
+                <div class="albumShadow">
+                    <div>
+                        <button>Edit</button>
+                    </div>
+                </div>
+            </div>
+            <div id="addAlbum">
+                <div>
+                    <button>Add Album</button>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+
+</html>
+<!-- description -->
+<!-- tags -->
+<!-- price -->
