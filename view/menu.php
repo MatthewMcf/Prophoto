93a6e9bcbd74e3e0f4db13665f96599ed4aa2092
harryjsutcliffe <harryjamessutcliffe@gmail.com>
2022-03-24 17:29:36 +0900

    merge with navMenu branch

--- view/menu.php
+++ view/menu.php
@@ -1,1 +1,35 @@
-<div>Menu to insert here</div>
+<header>
+
+    <?php $logged_in = TRUE;
+    
+    if ($logged_in) {
+    ?>
+    
+    <!-- Logged In -->
+    <div id="menuBar" class="loggedIn">
+        <div><img src="./public/images/ProPhoto.png" alt="Pro Photo Logo" id="siteLogo"></div>
+        <nav>
+            <button class="btnPrimary">My Profile</button>
+            <button class="btnSecondary">Logout</button>
+        </nav>
+    </div>
+
+
+
+    <?php } else {        
+    ?>
+
+    <!-- Logged Out -->
+    <div id="menuBar">
+        <div><img src="./public/images/ProPhoto.png" alt="Pro Photo Logo" id="siteLogo"></div>
+        <nav>
+            <button class="btnPrimary">Log In</button>
+            <button class="btnSecondary">Register</button>
+        </nav>
+    </div>
+
+
+
+    <?php } ?>
+</header>
+
