9a2a61d46e8968a71648009f3d3ed61aab1f9599
harryjsutcliffe <harryjamessutcliffe@gmail.com>
2022-03-24 17:24:21 +0900

    add new logo

--- view/menu.php
+++ view/menu.php
@@ -1,17 +1,17 @@
 <header>
 
-    <?php $logged_in = FALSE;
+    <?php $logged_in = TRUE;
     
     if ($logged_in) {
     ?>
     
     <!-- Logged In -->
     <div id="menuBar" class="loggedIn">
-    <div><img src="./public/images/ProPhoto.png" alt="Pro Photo Logo" id="siteLogo"></div>
-    <nav>
-        <button class="btnPrimary">My Profile</button>
-        <button class="btnSecondary">Logout</button>
-    </nav>
+        <div><img src="./public/images/ProPhoto.png" alt="Pro Photo Logo" id="siteLogo"></div>
+        <nav>
+            <button class="btnPrimary">My Profile</button>
+            <button class="btnSecondary">Logout</button>
+        </nav>
     </div>
 
 
@@ -21,11 +21,11 @@
 
     <!-- Logged Out -->
     <div id="menuBar">
-    <div><img src="./public/images/ProPhoto.png" alt="Pro Photo Logo" id="siteLogo"></div>
-    <nav>
-        <button class="btnPrimary">Log In</button>
-        <button class="btnSecondary">Register</button>
-    </nav>
+        <div><img src="./public/images/ProPhoto.png" alt="Pro Photo Logo" id="siteLogo"></div>
+        <nav>
+            <button class="btnPrimary">Log In</button>
+            <button class="btnSecondary">Register</button>
+        </nav>
     </div>
 
 
