732a6e477f8f0e27c5bf35d49e54c34ddb8589d3
leegihe95 <leegihe95@gmail.com>
2022-04-01 10:27:29 +0900

    Edited modalProfilePicture files

--- view/modalProfilePicture.php
+++ view/modalProfilePicture.php
@@ -15,12 +15,11 @@
     <div id="range-val"></div>-->
     <p id="message"></p>
     <div class="inputWrap">
-      <label class="inputPrimary" for="file">Upload your file</label>
-      <!--<input id="file" type="file" onchange="loadFile(event)">-->
+      <label class="btnSecondary" for="file">Upload your file</label>
       <input id="file" type="file" onchange="loadFile(event)">
       <!--<p>Your image has to be under 300kb. Only .png and .jpg files are supported.</p>-->
     </div>
-    <button class="btnSecondary updateButton">Update</button>
+    <button class="btnPrimary updateButton">Update</button>
   </div>
 </div>
 <!--</form>-->
