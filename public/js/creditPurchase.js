d4232bab9986755e8cc79fccde8b9de41d064495
Matt <m.w.mcfarlane87@gmail.com>
2022-04-01 12:20:24 +0900

    Recovered files and working again

--- public/js/creditPurchase.js
+++ public/js/creditPurchase.js
@@ -1,17 +1,17 @@
 window.onload = createYearOptions();
 
-// const form = document.getElementById("form");
+const form = document.getElementById("form");
 
-// // Check to see if needed
-// const cardHoldersName = document.getElementById("cardHoldersName");
-// const cardNumber = document.getElementById("cardNumber");
-// const securityCode = document.getElementById("securityCode");
-// const credits = document.getElementById("credits");
+// Check to see if needed
+const cardHoldersName = document.getElementById("cardHoldersName");
+const cardNumber = document.getElementById("cardNumber");
+const securityCode = document.getElementById("securityCode");
+const credits = document.getElementById("credits");
 
-// // Expire variables
-// const formTest = document.getElementById("formTest");
-// const expiryMonth = document.getElementById("expiryMonth");
-// const expiryYear = document.getElementById("expiryYear");
+// Expire variables
+const formTest = document.getElementById("formTest");
+const expiryMonth = document.getElementById("expiryMonth");
+const expiryYear = document.getElementById("expiryYear");
 
 // Get date and time for card experation
 var today = new Date();
@@ -19,7 +19,7 @@ var date =
 	today.getFullYear() + "-" + (today.getMonth() + 1) + "-" + today.getDate();
 
 // console.log(date);
-
+console.log("Test");
 form.addEventListener("submit", (e) => {
 	e.preventDefault();
 
@@ -47,24 +47,14 @@ selectCreditAmount();
 
 function checkInputs() {
 	// trim to remove the whitespaces
-	const creditsValue = credits.value;
 	const cardHoldersNameValue = cardHoldersName.value.trim();
 	const cardNumberValue = cardNumber.value.trim();
 	const securityCodeValue = securityCode.value.trim();
-
-	// Look at why I have created duplicates of variables
-	// Expire
-	const expiryMonthValue = expiryMonth.value;
-	const expiryYearValue = expiryYear.value;
 
 	// Date
 	const month = expiryMonth.value;
 	const year = expiryYear.value;
 
-	// Create a date object from month and year, on the first
-	// of that month. You could check the end of the month
-	// but that would be a little more complicated as you'd need
-	// to know how many days are in that month.
 	const expiryDate = new Date(year + "-" + month + "-01");
 
 	// Credit error condition
@@ -142,156 +132,7 @@ function createYearOptions() {
 	document.querySelector("#expiryYear").innerHTML = yearOptions;
 }
 
-// function setSuccessFor(input) {
-// 	const form = input.parentElement;
-// 	// formControl.className = "inputContainer success";
-// }
-
-// // // // // // // Junk to hold on too // // // // // // //
-
-// Expire test
-// const formTest = document.getElementById('formTest');
-// const expiryMonth = document.getElementById('expiryMonth');
-// const expiryYear = document.getElementById('expiryYear');
-
-// form.addEventListener('submit', ev => {
-//   ev.preventDefault();
-
-//   const month = expiryMonth.value;
-//   const year = expiryYear.value;
-
-//   // Create a date object from month and year, on the first
-//   // of that month. You could check the end of the month
-//   // but that would be a little more complicated as you'd need
-//   // to know how many days are in that month.
-//   const expiryDate = new Date(year + '-' + month + '-01');
-
-//   // You can compare date objects, this says if the expiryDate is
-//   // less than todays date, i.e. in the past. You could do <= if
-//   // you want to check whether they're the same date aswell.
-//   if (expiryDate < new Date()) {
-//     // Fails validation, show some error message to user
-//     console.log('fail')
-//   } else {
-//     // Valid expiry
-//     console.log('pass')
-//   }
-// })
-
-// // Selecting the amount of tokens
-// function selectCreditAmount () {
-//     const creditCost = 5;
-//     const select = document.querySelector("select");
-//     let totalAmount = document.querySelector("#totalAmount");
-//     let numbOfCredits = document.querySelector("#numbOfCredits");
-//     let totalCost = document.querySelector("#totalCost");
-
-//     select.addEventListener('change', () => {
-//         numbOfCredits = select.value;
-//         totalAmount = select.value * creditCost;
-//         totalCost.textContent = `Total cost of ${numbOfCredits} credits : $${totalAmount}`;
-
-//         return totalAmount;
-//     });
-// }
-
-// selectCreditAmount ();
-
-// console.log(totalAmount);
-
-// const cardholdersName = document.getElementById('cardholdersName');
-// const cardNumber = document.getElementById('cardNumber');
-// const form = document.getElementById('form');
-// const errorCardholdersName = document.getElementById('errorCardholdersName');
-// const errorCardNumber = document.getElementById('errorCardNumber');
-
-// form.addEventListener('submit', (e) => {
-//     let cardholdersNameMessages = [];
-//     let cardNumberMessages = [];
-
-//     // Card holders name
-//     if (cardholdersName.value === '' || cardholdersName.value == null) {
-//         cardholdersNameMessages.push('You must enter the cardholders name');
-//     }
-
-//     // Card number
-//     if (cardNumber.value === '' || cardNumber.value == null) {
-//         cardNumberMessages.push('You must enter the card number');
-//     }
-
-//     if (cardNumber.value.length < 15) {
-//         cardNumberMessages.push('Card number must be at least 15 numbers');
-//     }
-
-//     if (cardNumber.value.length >= 16) {
-//         cardNumberMessages.push('Card number must have a maximum of 16 numbers');
-//     }
-
-//     // Submit payment
-//     if (cardNumberMessages.length > 0) {
-//         e.preventDefault();
-//         errorCardholdersName.innerText = cardholdersNameMessages.join(', ');
-//         errorCardNumber.innerText = cardNumberMessages.join(', ');
-//     }
-
-// })
-
-//     const submit = document.getElementById("submit");
-
-//     submit.addEventListener("click", validate);
-
-//     function validate(e) {
-//     e.preventDefault();
-
-//     const firstNameField = document.getElementById("firstname");
-//     const firstNameLabel = document.getElementById("firstname-label");
-
-//     const lastNameField = document.getElementById("lastname");
-//     const lastNameLabel = document.getElementById("lastname-label");
-//     let valid = true;
-
-//     if (!firstNameField.value) {
-//         const nameError = document.getElementById("nameError");
-
-//         nameError.classList.add("visible");
-//         firstNameField.classList.add("invalid");
-//         firstNameLabel.classList.add("invalid-label");
-//         nameError.classList.add("invalid-label");
-
-//         nameError.setAttribute("error-hidden", false);
-//         nameError.setAttribute("error-invalid", true);
-
-//         nameError.textContent = "Please enter First Name";
-//     } else {
-//         nameError.classList.remove("visible");
-//         firstNameField.classList.remove("invalid");
-//         firstNameLabel.classList.remove("invalid-label");
-
-//         nameError.setAttribute("error-hidden", true);
-//         nameError.setAttribute("error-invalid", false);
-//         return valid;
-//     }
-
-//     if (!lastNameField.value) {
-//         const nameError = document.getElementById("lastnameError");
-
-//         nameError.classList.add("visible");
-//         lastNameField.classList.add("invalid");
-//         lastNameLabel.classList.add("invalid-label");
-//         nameError.classList.add("invalid-label");
-
-//         nameError.setAttribute("error-hidden", false);
-//         nameError.setAttribute("error-invalid", true);
-
-//         nameError.textContent = "Please enter First Name";
-//     } else {
-//         nameError.classList.remove("visible");
-//         lastNameField.classList.remove("invalid");
-//         lasttNameLabel.classList.remove("invalid-label");
-
-//         nameError.setAttribute("error-hidden", true);
-//         nameError.setAttribute("error-invalid", false);
-//         return valid;
-//     }
-
-// }
+function setSuccessFor(input) {
+	const inputSuccess = input.parentElement;
+	inputSuccess.className = "inputContainer";
+}
--- view/creditPurchaseView.php
+++ view/creditPurchaseView.php
@@ -1,403 +1,77 @@
-<?php //ob_start(); ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../public/css/style.css">
-    <link rel="stylesheet" href="../public/css/creditPurchase.css">
-    <script
-			src="https://kit.fontawesome.com/68fcd009c8.js"
-			crossorigin="anonymous"
-		></script>
-    <title>Document</title>
-</head>
-<body onload="createYearOptions()">
-    <div class="container">
-        <h1>Purchase credits</h1>
+<!-- <div class="modalSection"> -->
+<?php //session_start();
+?>
+<?php ob_start(); ?>
+<!-- Can't have this in body -->
+<!-- <body onload="createYearOptions()"> -->
+<link rel="stylesheet" href="../public/css/style.css">
+<link rel="stylesheet" href="../public/css/creditPurchase.css">
+<section>
+    <h1>Purchase credits</h1>
+
+    <form action="connection.php" method="post" id="form" class="form">
+        <div class="inputContainer">
+            <label for="credits">Amount of credits</label>
+            <p>1 credit costs $5</p>
+            <select name="credits" id="credits">
+                <option value="0" selected="true" disabled="disabled">Choose an amount</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="10">10</option>
+                <option value="15">15</option>
+            </select>
+            <small>Error message</small>
+            <h3 id="totalCost"></h3>
+        </div>
+
+        <div class="inputContainer">
+            <label for="cardHoldersName">Card holders name</label>
+            <input type="text" placeholder="Card holders name" id="cardHoldersName">
+            <small>Error message</small>
+        </div>
+
+        <div class="inputContainer">
+            <label for="cardNumber">Card number</label>
+            <input type="number" placeholder="1234 1234 1234 1234" id="cardNumber">
+            <small>Error message</small>
+        </div>
+
+        <div class="inputContainer">
+            <label> Expiry MM/YY: </label>
+            <select id="expiryMonth">
+                <option selected value="0"> Month </option>
+                <option value="1">01</option>
+                <option value="2">02</option>
+                <option value="3">03</option>
+                <option value="4">04</option>
+                <option value="5">05</option>
+                <option value="6">06</option>
+                <option value="7">07</option>
+                <option value="8">08</option>
+                <option value="9">09</option>
+                <option value="10">10</option>
+                <option value="11">11</option>
+                <option value="12">12</option>
+            </select>
+
+            <select id="expiryYear">       
+            </select>
+            <small>Error message</small>
+        </div>
+
+        <div class="inputContainer">
+            <label for="securityCode">Security code</label>
+            <input type="number" placeholder="123" id="securityCode">
+            <small>Error message</small>
+        </div>
+
+        <button class="btnPrimary">Buy now</button>
+    </form>	
+</section>
+<script type="text/javascript" src="../public/js/creditPurchase.js"></script>
+<?php $content = ob_get_clean(); ?>
+<?php require('template.php'); ?>
 
-        <form id="form" class="form">
-
-            <div class="inputContainer">
-                <label for="credits">Amount of credits</label>
-                <p>1 credit costs $5</p>
-                <select name="credits" id="credits">
-                    <option value="0" selected="true" disabled="disabled">Choose an amount</option>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                    <option value="10">10</option>
-                    <option value="15">15</option>
-                </select>
-                <small>Error message</small>
-                <h3 id="totalCost"></h3>
-            </div>
-
-            <div class="inputContainer">
-                <label for="cardHoldersName">Card holders name</label>
-                <input type="text" placeholder="Card holders name" id="cardHoldersName">
-                <small>Error message</small>
-            </div>
-            <div class="inputContainer">
-                <label for="cardNumber">Card number</label>
-                <input type="number" placeholder="1234 1234 1234 1234" id="cardNumber">
-                <small>Error message</small>
-            </div>
-
-            <h4>Expiration date</h4>
-            <h4>Month</h4>
-            <select name="month" id="month">
-                <option value="0">Select a month</option>
-                <option value="1">01 January</option>
-                <option value="2">02 February</option>
-                <option value="3">03 March</option>
-                <option value="4">04 April</option>
-                <option value="5">05 May</option>
-                <option value="6">06 June</option>
-                <option value="7">07 July</option>
-                <option value="8">08 August</option>
-                <option value="9">09 September</option>
-                <option value="10">10 October</option>
-                <option value="11">11 November</option>
-                <option value="12">12 December</option>
-            </select> 
-
-            <h4>Year</h4>
-            <select name="year" id="year">
-                
-                <!-- <option id="year1" value="1"></option>
-                <option id="year2" value="2"></option> -->
-            </select>           
-
-            <div class="inputContainer">
-                <label for="securityCode">Security code</label>
-                <input type="number" placeholder="123" id="securityCode">
-                <small>Error message</small>
-            </div>
-
-            <button>Submit</button>
-
-    
-        </form>
-
-    </div>
-
-    <script>
-
-    const form = document.getElementById('form');
-
-    // Check to see if needed
-    const cardHoldersName = document.getElementById('cardHoldersName');
-    const cardNumber = document.getElementById('cardNumber');
-    const securityCode = document.getElementById('securityCode');
-    const credits = document.getElementById('credits');
-   
-
-    // Get date and time for card experation
-    var today = new Date();
-    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-
-    console.log(date);
-
-    form.addEventListener('submit', e => {
-        e.preventDefault();
-        
-        checkInputs();
-    });
-
-    // Selecting the amount of tokens
-    function selectCreditAmount () {
-        const creditCost = 5;
-        const select = document.querySelector("select");
-        let totalAmount = document.querySelector("#totalAmount");
-        let numbOfCredits = document.querySelector("#numbOfCredits");
-        let totalCost = document.querySelector("#totalCost");
-
-        select.addEventListener('change', () => {       
-            numbOfCredits = select.value;
-            totalAmount = select.value * creditCost;  
-            totalCost.textContent = `Total cost of ${numbOfCredits} credits : $${totalAmount}`;
-
-            return totalAmount;
-        });
-    }
-
-    selectCreditAmount ();
-
-    function checkInputs() {
-        // trim to remove the whitespaces
-        const creditsValue = credits.value;
-        const cardHoldersNameValue = cardHoldersName.value.trim();
-        const cardNumberValue = cardNumber.value.trim();
-        const securityCodeValue = securityCode.value.trim();
-
-        // Credit error condition
-        if(creditsValue == 0) {
-            setErrorFor(credits, 'Please select how many credits you would like to purchase');
-        } else {
-            setSuccessFor(credits);
-        }
-
-        // Cardholder's name error condition
-        if(cardHoldersNameValue === '') {
-            setErrorFor(cardHoldersName, 'Card holders name cannot be blank');
-        } else {
-            setSuccessFor(cardHoldersName);
-        }
-
-        // Card number error condition
-        if(cardNumberValue === '') {
-            setErrorFor(cardNumber, 'Card holders name cannot be blank');
-        } else if (cardNumber.value.length < 15) {
-            setErrorFor(cardNumber, 'Card number can not be less than 15 numbers');
-        } else if (cardNumber.value.length > 16) {
-            setErrorFor(cardNumber, 'Card number can not be more than 16 numbers');
-        } else {
-            setSuccessFor(cardNumber);
-        }
-
-        // Security code error condition
-        if(securityCodeValue === '') {
-            setErrorFor(securityCode, 'Security code cannot be blank');
-        } else if (securityCode.value.length < 3) {
-            setErrorFor(securityCode, 'Security code can not be less than 3 numbers');
-        } else if (securityCode.value.length > 3) {
-            setErrorFor(securityCode, 'Security code can not be more than 3 numbers');
-        } else {
-            setSuccessFor(securityCode);
-        }  
-    }
-
-    function setErrorFor(input, message) {
-        const inputContainer = input.parentElement;
-        const small = inputContainer.querySelector('small');
-        inputContainer.className = 'inputContainer error';
-        small.innerText = message;
-    }
-
-function createYearOptions() {
-    var newDate = new Date();
-    var currentYear = newDate.getFullYear();
-    var yearOptions = "<option value='0'>Select</option>";
-
-    for (var i = currentYear; i < currentYear + 10; i++) {
-        yearOptions += "<option value='"+i+"'>"+i+"</option>"
-    }
-
-    document.querySelector("#year").innerHTML = yearOptions;
-}
-
-
-// var sel = document.getElementById("year");
-
-// var year1 = document.createElement("option");
-// var year2 = document.createElement("option");
-
-// const d = new Date();
-// let year = d.getFullYear();
-// // document.getElementById("demo").innerHTML = year;
-
-// year1.value = "1";
-// year1.text = year;
-
-// year2.value = "2";
-// year2.text = year + 1;
-
-// sel.add(year1, null);
-// sel.add(year2, null);
-
-
-
-
-
-    // function setSuccessFor(input) {
-    //     const formControl = input.parentElement;
-    //     formControl.className = 'inputContainer success';
-    // }
-        
-    // // Selecting the amount of tokens
-    // function selectCreditAmount () {
-    //     const creditCost = 5;
-    //     const select = document.querySelector("select");
-    //     let totalAmount = document.querySelector("#totalAmount");
-    //     let numbOfCredits = document.querySelector("#numbOfCredits");
-    //     let totalCost = document.querySelector("#totalCost");
-
-    //     select.addEventListener('change', () => {       
-    //         numbOfCredits = select.value;
-    //         totalAmount = select.value * creditCost;  
-    //         totalCost.textContent = `Total cost of ${numbOfCredits} credits : $${totalAmount}`;
-
-    //         return totalAmount;
-    //     });
-    // }
-
-    // selectCreditAmount ();
-    
-    // console.log(totalAmount);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // const cardholdersName = document.getElementById('cardholdersName');
-    // const cardNumber = document.getElementById('cardNumber');
-    // const form = document.getElementById('form');
-    // const errorCardholdersName = document.getElementById('errorCardholdersName');
-    // const errorCardNumber = document.getElementById('errorCardNumber');
-
-    // form.addEventListener('submit', (e) => {
-    //     let cardholdersNameMessages = [];
-    //     let cardNumberMessages = [];
-
-    //     // Card holders name
-    //     if (cardholdersName.value === '' || cardholdersName.value == null) {
-    //         cardholdersNameMessages.push('You must enter the cardholders name');
-    //     }
-
-    //     // Card number
-    //     if (cardNumber.value === '' || cardNumber.value == null) {
-    //         cardNumberMessages.push('You must enter the card number');
-    //     } 
-
-    //     if (cardNumber.value.length < 15) {
-    //         cardNumberMessages.push('Card number must be at least 15 numbers');
-    //     }
-
-    //     if (cardNumber.value.length >= 16) {
-    //         cardNumberMessages.push('Card number must have a maximum of 16 numbers');
-    //     }
-
-    //     // Submit payment
-    //     if (cardNumberMessages.length > 0) {
-    //         e.preventDefault();
-    //         errorCardholdersName.innerText = cardholdersNameMessages.join(', ');
-    //         errorCardNumber.innerText = cardNumberMessages.join(', ');
-    //     }
-        
-    // })
-
-
-    //     const submit = document.getElementById("submit");
-
-    //     submit.addEventListener("click", validate);
-
-    //     function validate(e) {
-    //     e.preventDefault();
-
-    //     const firstNameField = document.getElementById("firstname");
-    //     const firstNameLabel = document.getElementById("firstname-label");
-
-    //     const lastNameField = document.getElementById("lastname");
-    //     const lastNameLabel = document.getElementById("lastname-label");
-    //     let valid = true;
-
-    //     if (!firstNameField.value) {
-    //         const nameError = document.getElementById("nameError");
-
-    //         nameError.classList.add("visible");
-    //         firstNameField.classList.add("invalid");
-    //         firstNameLabel.classList.add("invalid-label");
-    //         nameError.classList.add("invalid-label");
-
-    //         nameError.setAttribute("error-hidden", false);
-    //         nameError.setAttribute("error-invalid", true);
-
-    //         nameError.textContent = "Please enter First Name";
-    //     } else {
-    //         nameError.classList.remove("visible");
-    //         firstNameField.classList.remove("invalid");
-    //         firstNameLabel.classList.remove("invalid-label");
-
-    //         nameError.setAttribute("error-hidden", true);
-    //         nameError.setAttribute("error-invalid", false);
-    //         return valid;
-    //     }
-
-    //     if (!lastNameField.value) {
-    //         const nameError = document.getElementById("lastnameError");
-
-    //         nameError.classList.add("visible");
-    //         lastNameField.classList.add("invalid");
-    //         lastNameLabel.classList.add("invalid-label");
-    //         nameError.classList.add("invalid-label");
-
-    //         nameError.setAttribute("error-hidden", false);
-    //         nameError.setAttribute("error-invalid", true);
-
-    //         nameError.textContent = "Please enter First Name";
-    //     } else {
-    //         nameError.classList.remove("visible");
-    //         lastNameField.classList.remove("invalid");
-    //         lasttNameLabel.classList.remove("invalid-label");
-
-    //         nameError.setAttribute("error-hidden", true);
-    //         nameError.setAttribute("error-invalid", false);
-    //         return valid;
-    //     }
-           
-    // }
-
-        // Error messages
-
-    //     function errorMessage() {
-    //     var error = document.getElementById("error")
-    //     if (isNaN(document.getElementById("cardNumber").value))
-    //     {
-    //         // Changing content and color of content
-    //         error.textContent = "Please enter a valid number"
-    //         error.style.color = "red"
-    //     } else {
-    //         error.textContent = ""
-    //     }
-    // }
-
-        // // Selecting the amount of tokens
-        // function selectCreditAmount () {
-        //     const creditCost = 5;
-        //     const select = document.querySelector("select");
-        //     let totalAmount = document.querySelector("#totalAmount");
-        //     let numbOfCredits = document.querySelector("#numbOfCredits");
-        //     let totalCost = document.querySelector("#totalCost");
-
-        //     select.addEventListener('change', () => {       
-        //         numbOfCredits = select.value;
-        //         totalAmount = select.value * creditCost;  
-        //         totalCost.textContent = `Total cost of ${numbOfCredits} credits : $${totalAmount}`;
-
-        //         return totalAmount;
-        //     });
-        // }
-    
-        // selectCreditAmount ();
-        
-        // console.log(totalAmount);
-
-	</script>
-
-</body>
-</html>
-
-
-
-
-
-
-<?php //$content = ob_get_clean(); ?>
-<?php //require('template.php'); ?>
