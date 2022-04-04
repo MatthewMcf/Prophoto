2341989abdb0760d7b1a66e59fb40053efa230d4
leegihe95 <leegihe95@gmail.com>
2022-04-04 12:01:35 +0900

    edited CSS for modalPhotoEdit.php

--- view/cardView.php
+++ view/cardView.php
@@ -2,7 +2,7 @@
     <div class="cardContent" style="background-image: url(<?=$card["path"]?>)" image-id=<?=$card["id"]?>>
         <button class="price"><?=$card["price"] ? $card["price"] : "2"?> Credits</button>
         <div>
-            <button class="editPic btnHollowSecondary" image-id=<?=$card["id"]?>>Edit</button>
+            <button class="editPic btnHollowSecondary" image-id=<?=$card["id"]?>><i class="fa-solid fa-pen-to-square"></i></button>
         </div>
     </div>
 </div>
