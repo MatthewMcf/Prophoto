e4e8d871a8547b7d5b958c0b8be3e9255077c7cc
leegihe95 <leegihe95@gmail.com>
2022-04-04 14:29:53 +0900

    pictureUpload CSS halfway

--- view/modalPictureUpload.php
+++ view/modalPictureUpload.php
@@ -1,4 +1,4 @@
-<!--<link rel="stylesheet" href="./public/css/modalProfilePicture.css">-->
+<link rel="stylesheet" href="./public/css/modalProfilePicture.css">
 <!--<form action="#">-->
 <div class="modalSection">
 <?php 
@@ -6,21 +6,20 @@
     session_start(); 
   }
  ?>
-  <h2>Upload your picture</h2>
-  <div class="imgPreview">
-    <img id="modalUploadedImage" src="" width="200" />
+  <h2>Upload a picture</h2>
+  <div class="imgPreviewUpload">
+    <img id="modalUploadedImage" src="" width="500" />
   </div>
   <div>
     <!--<input type="range" min="0" max="9" step="1" value="0" id="zoom">
     <div id="range-val"></div>-->
     <p id="message"></p>
     <div class="inputWrap">
-        <label class="inputPrimary" for="file">Upload your file</label>
-    <!--<input id="file" type="file" onchange="loadFile(event)">-->
+        <label id="upload" class="btnSecondary" for="file">Choose a file</label>
         <input id="file" type="file" onchange="loadUploadedImage(event)">
         <!--<p>Your image has to be under 300kb. Only .png and .jpg files are supported.</p>-->
     </div>
-    <button class="btnSecondary updateButton">Upload</button>
+    <button class="btnPrimary updateButton">Upload</button>
   </div>
 </div>
 <!--</form>-->
