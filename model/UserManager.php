9a48692b5136103a8507a2e9df93ad04abab0c1d
leegihe95 <leegihe95@gmail.com>
2022-04-05 14:29:25 +0900

    fixed aboutme code

--- model/UserManager.php
+++ model/UserManager.php
@@ -218,7 +218,7 @@ class UserManager extends Manager
     public function setUserInfo($params) {
         $stmt = $this->_connection->prepare("UPDATE `users` SET `display_name`=?,`about_me`=?,`website`=?,`facebook`=?,`instagram`=?,`linkedin`=? WHERE id = ?");
         $stmt->bindParam(1, $params["name"], PDO::PARAM_STR);
-        $stmt->bindParam(2, $params["aboutme"], PDO::PARAM_STR);
+        $stmt->bindParam(2, $params["aboutMe"], PDO::PARAM_STR);
         $stmt->bindParam(3, $params["website"], PDO::PARAM_STR);
         $stmt->bindParam(4, $params["facebook"], PDO::PARAM_STR);
         $stmt->bindParam(5, $params["instagram"], PDO::PARAM_STR);
