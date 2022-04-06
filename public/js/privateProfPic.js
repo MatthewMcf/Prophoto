d452da9a10a4134c85802656b717ef708e589f26
leegihe95 <leegihe95@gmail.com>
2022-04-06 10:56:56 +0900

    fixed private profile albums alignment

--- public/js/privateProfPic.js
+++ public/js/privateProfPic.js
@@ -2,44 +2,44 @@
 // Trigger Modal to edit Profile Picture
 let profilePic = document.getElementById("profilePic");
 profilePic.addEventListener("click", function () {
-    setModalContent();
+	setModalContent();
 });
 
 // Trigger Modal to edit Profile Info
 let profileInfo = document.getElementById("editProfileInfo");
 profileInfo.addEventListener("click", function () {
-    setModalProfileInfo();
+	setModalProfileInfo();
 });
 
 // Trigger Modal to edit Picture
 let editPic = document.querySelectorAll(".editPic");
 for (let i = 0; i < editPic.length; i++) {
-    editPic[i].addEventListener("click", function (e) {
-        e.stopPropagation();
-        setModalEditPic(e);
-    });
+	editPic[i].addEventListener("click", function (e) {
+		e.stopPropagation();
+		setModalEditPic(e);
+	});
 }
 
 // Trigger accordion effect on Photos
 let arrowDowns = document.querySelectorAll(".sectionHeader");
 console.log(arrowDowns);
 for (let i = 0; i < arrowDowns.length; i++) {
-    arrowDowns[i].addEventListener("click", function () {
-        this.lastElementChild.classList.toggle("rotate");
+	arrowDowns[i].addEventListener("click", function () {
+		this.lastElementChild.classList.toggle("rotate");
 
-        let sectionPhotos = this.nextElementSibling;
-        if (sectionPhotos.style.maxHeight) {
-            sectionPhotos.style.maxHeight = null;
-        } else {
-            sectionPhotos.style.maxHeight = sectionPhotos.scrollHeight + "px";
-        }
-    });
+		let sectionPhotos = this.nextElementSibling.firstElementChild;
+		if (sectionPhotos.style.maxHeight) {
+			sectionPhotos.style.maxHeight = null;
+		} else {
+			sectionPhotos.style.maxHeight = sectionPhotos.scrollHeight + "px";
+		}
+	});
 }
 
 // Trigger Modal to upload a photo
 let addPicture = document.querySelector("#addPicture");
 addPicture.addEventListener("click", function () {
-    setModalUploadPic();
+	setModalUploadPic();
 });
 
 // Show more cards of user's images
