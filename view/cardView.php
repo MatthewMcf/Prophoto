13b5897486c9a4544313fa64ddda0f4c823b4f54
leegihe95 <leegihe95@gmail.com>
2022-04-06 12:52:09 +0900

    revised buttons in uploaded pictures

--- view/cardView.php
+++ view/cardView.php
@@ -2,8 +2,10 @@
     <div class="cardContent" style="background-image: url(<?=$card["path"]?>)" image-id=<?=$card["id"]?>>
         <div id="cardButtons">
             <button class="price"><?=$card["price"] ? $card["price"] : "2"?> Credits</button>
-            <button class="editPic btnHollowSecondary" image-id=<?=$card["id"]?>><i class="fa-solid fa-pen-to-square"></i></button>
-            <button" id="delete" class="btnSecondary deleteButton" onclick="deleteExistingImage(<?= $card['id'] ?>, <?= $card['userID'] ?> ); event.stopPropagation();"><i class="fa-solid fa-trash-can"></i></button>
+            <div>
+                <button class="editPic btnHollowSecondary" image-id=<?=$card["id"]?>><i class="fa-solid fa-pen-to-square"></i></button>
+                <button" id="delete" class="btnSecondary deleteButton" onclick="deleteExistingImage(<?= $card['id'] ?>, <?= $card['userID'] ?> ); event.stopPropagation();"><i class="fa-solid fa-trash-can"></i></button>
+            </div>
         </div>
     </div>
 </div>
