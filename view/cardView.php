a80bda005180e90e3f5d782251488fe452d17cde
leegihe95 <leegihe95@gmail.com>
2022-04-08 12:46:34 +0900

    fixed base price

--- view/cardView.php
+++ view/cardView.php
@@ -1,7 +1,7 @@
 <div class="cardContainer">
     <div class="cardContent" style="background-image: url(<?=$card["path"]?>)" image-id=<?=$card["id"]?>>
         <div id="cardButtons">
-            <button class="price"><?=$card["price"] ? $card["price"] : "2"?> Credits</button>
+            <button class="price"><?=$card["price"] ? $card["price"] : "5"?> Credits</button>
             <div>
                 <button class="editPic btnHollowSecondary" image-id=<?=$card["id"]?>><i class="fa-solid fa-pen-to-square"></i></button>
                 <button" id="delete" class="btnSecondary deleteButton" onclick="deleteExistingImage(<?= $card['id'] ?>, <?= $card['userID'] ?> ); event.stopPropagation();"><i class="fa-solid fa-trash-can"></i></button>
